f2050dff833ed3b7d0d7faaf0279360895b45b11
higuaifan <higuaifan@higuaifan.com>
2022-04-22 11:48:40 +0800

    🔧 修改vitest配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include:['test/**/**.test.ts']
+    include: ['test/**/**.test.ts'],
+    coverage: {
+      reporter: ['json', 'html']
+    }
   },
 })
 
